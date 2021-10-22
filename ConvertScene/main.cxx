9e6b878078f1114d8e84b9f91763e62c427e48f5
llq <llq@gmail.com>
2021-10-22 16:05:41 +0800

    rename remove

--- ConvertScene/main.cxx
+++ ConvertScene/main.cxx
@@ -28,88 +28,135 @@
 
 #include <fbxsdk.h>
 
-#include "../Common/Common.h"
-
-#define SAMPLE_FILENAME "box.fbx"
-
-const char* lFileTypes[] =
+void ProcessNode(FbxNode* pNode)
 {
-    "_dae.dae",            "Collada DAE (*.dae)",
-    "_fbx7binary.fbx", "FBX binary (*.fbx)",
-    "_fbx7ascii.fbx",  "FBX ascii (*.fbx)",
-    "_fbx6binary.fbx", "FBX 6.0 binary (*.fbx)",
-    "_fbx6ascii.fbx",  "FBX 6.0 ascii (*.fbx)",
-    "_obj.obj",            "Alias OBJ (*.obj)",
-    "_dxf.dxf",            "AutoCAD DXF (*.dxf)"
-};
+    FBXSDK_printf(pNode->GetName());
+    FBXSDK_printf("\n");
+
+    if (pNode->GetNodeAttribute())
+    {
+        switch (pNode->GetNodeAttribute()->GetAttributeType())
+        {
+        case FbxNodeAttribute::eMesh:
+            //ProcessMesh(pNode);
+            FBXSDK_printf("eMesh.\n");
+            break;
+
+        case FbxNodeAttribute::eSkeleton:
+            //ProcessSkeleton(pNode);
+            FBXSDK_printf("eSkeleton.\n");
+            break;
+
+        case FbxNodeAttribute::eLight:
+            //ProcessLight(pNode);
+            FBXSDK_printf("eLight.\n");
+            break;
+
+        case FbxNodeAttribute::eCamera:
+            //ProcessCamera();
+            FBXSDK_printf("eCamera.\n");
+            break;
+
+        default:
+            FBXSDK_printf("default\n");
+            break;
+        }
+    }
+
+    if (pNode == NULL)
+        return;
+
+    for (int i = 0; i < pNode->GetChildCount(); ++i)
+    {
+        ProcessNode(pNode->GetChild(i));
+    }
+}
 
 int main(int argc, char** argv)
 {
-	FbxString lFilePath("");
-	for( int i = 1, c = argc; i < c; ++i )
-	{
-		if( FbxString(argv[i]) == "-test" ) continue;
-		else if( lFilePath.IsEmpty() ) lFilePath = argv[i];
-	}
-	if( lFilePath.IsEmpty() ) lFilePath = SAMPLE_FILENAME;
-
-    FbxManager* lSdkManager = NULL;
-    FbxScene* lScene = NULL;
-
-    // Prepare the FBX SDK.
-    InitializeSdkObjects(lSdkManager, lScene);
-
-	bool lResult = LoadScene(lSdkManager, lScene, lFilePath.Buffer());
-    if( lResult )
-    {		
-		const size_t lFileNameLength = strlen((argc>=3)?argv[2]:lFilePath.Buffer());
-        char* lNewFileName = new char[lFileNameLength+64];
-        FBXSDK_strcpy(lNewFileName,lFileNameLength+64,(argc>=3)?argv[2]:lFilePath.Buffer());
-
-        const size_t lFileTypeCount = sizeof(lFileTypes)/sizeof(lFileTypes[0])/2;
-
-        for(size_t i=0; i<lFileTypeCount; ++i)
-        {
-            // Retrieve the writer ID according to the description of file format.
-            int lFormat = lSdkManager->GetIOPluginRegistry()->FindWriterIDByDescription(lFileTypes[i*2+1]);
-
-            // Construct the output file name.
-            FBXSDK_strcpy(lNewFileName+lFileNameLength-4,60, lFileTypes[i*2]);
-
-            // Create an exporter.
-            FbxExporter* lExporter = FbxExporter::Create(lSdkManager, "");
-
-            // Initialize the exporter.
-			lResult = lExporter->Initialize(lNewFileName, lFormat, lSdkManager->GetIOSettings());
-            if( !lResult )
-            {
-                FBXSDK_printf("%s:\tCall to FbxExporter::Initialize() failed.\n", lFileTypes[i*2+1]);
-                FBXSDK_printf("Error returned: %s\n\n", lExporter->GetStatus().GetErrorString());
-            }
-            else
-            {
-                // Export the scene.
-				lResult = lExporter->Export(lScene);
-                if( !lResult )
-                {
-                    FBXSDK_printf("Call to FbxExporter::Export() failed.\n");
-                }
-            }
-
-            // Destroy the exporter.
-            lExporter->Destroy();
-        }
-        delete[] lNewFileName;
-    }
-	else
+    if (argc == 1)
     {
-        FBXSDK_printf("Call to LoadScene() failed.\n");
+        FBXSDK_printf("fbxPath rename/remove name, [name2]\n");
+        return 0;
     }
+
+    FbxString fbxPath(argv[1]);
+
+    FbxManager* pManager = FbxManager::Create();
+
+    //Create an IOSettings object. This object holds all import/export settings.
+    FbxIOSettings* ios = FbxIOSettings::Create(pManager, IOSROOT);
+    pManager->SetIOSettings(ios);
+
+    //Load plugins from the executable directory (optional)
+    pManager->LoadPluginsDirectory(FbxGetApplicationDirectory().Buffer());
+
+    //Create an FBX scene. This object holds most objects imported/exported from/to files.
+    FbxScene* pScene = FbxScene::Create(pManager, "My Scene");
+
+    // Create an importer.
+    FbxImporter* pImporter = FbxImporter::Create(pManager, "");
+
+    // Initialize the importer by providing a filename.
+    pImporter->Initialize(fbxPath.Buffer(), -1, pManager->GetIOSettings());
+
+    // Import the scene.
+    pImporter->Import(pScene);
+
+    // Destroy the importer.
+    pImporter->Destroy();
+
+    //ProcessNode(pScene->GetRootNode());
+
+    if (FbxString(argv[2]) == FbxString("rename"))
+    {
+        for (int i = 0; i < pScene->GetSrcObjectCount<FbxAnimStack>(); i++)
+        {
+            FbxAnimStack* pAnimStack = pScene->GetSrcObject<FbxAnimStack>(i);
+
+            if (FbxString(pAnimStack->GetName()) == FbxString(argv[3]))
+            {
+                pAnimStack->SetName(argv[4]);
+                break;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < pScene->GetSrcObjectCount<FbxAnimStack>(); i++)
+        {
+            FbxAnimStack* pAnimStack = pScene->GetSrcObject<FbxAnimStack>(i);
+
+            if (FbxString(pAnimStack->GetName()) == FbxString(argv[3]))
+            {
+                pAnimStack->Destroy();
+                break;
+            }
+        }
+    }
+
+    //pAnimStack->LocalStart = (FbxTime)pAnimStack->LocalStop / 2;
+    //pAnimStack->LocalStop = (FbxTime)pAnimStack->LocalStart + (FbxTime)pAnimStack->LocalStop / 4;
+
+    // Retrieve the writer ID according to the description of file format.
+    int lFormat = pManager->GetIOPluginRegistry()->FindWriterIDByDescription("FBX binary (*.fbx)");
+
+    // Create an exporter.
+    FbxExporter* pExporter = FbxExporter::Create(pManager, "");
+
+    // Initialize the exporter.
+    pExporter->Initialize(fbxPath.Left(fbxPath.GetLen() - 4) + "_out.fbx", lFormat, pManager->GetIOSettings());
+    // Export the scene.
+    pExporter->Export(pScene);
+
+    // Destroy the exporter.
+    pExporter->Destroy();
 
     // Delete the FBX SDK manager. All the objects that have been allocated 
     // using the FBX SDK manager and that haven't been explicitly destroyed 
     // are automatically destroyed at the same time.
-    DestroySdkObjects(lSdkManager, lResult);
+    pManager->Destroy();
+
     return 0;
 }
 
